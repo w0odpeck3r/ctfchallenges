c6a98606c869ed59057880d43c41154f6d3415ae
w0odpeck3r <54020499+w0odpeck3r@users.noreply.github.com>
2019-08-22 23:56:05 +0800

    Update ret2text.c

--- pwn/ret2text.c
+++ pwn/ret2text.c
@@ -10,42 +10,39 @@
 #include <stdlib.h>
 
 void init(){
-	int fd;
-	setvbuf(stdin,0,2,0);
-	setvbuf(stdout,0,2,0);
-	setvbuf(stderr,0,2,0);
-	
-	fd = open("/dev/urandom",0);
-	close(fd);
-
-	fd = open("./flag",O_RDONLY);
-	if(fd==-1){
-		puts("where is the flag?\n");
-		exit(0);
-	}
-	close(fd);
-
-	puts("欢迎各位2019级新生来到山东科技大学(泰安)·信息安全协会！");
-	puts("非常期待能遇见喜欢pwn的新生23333");
-	puts("");
-	puts("简单的栈溢出~");
+    int fd;
+    setvbuf(stdin,0,2,0);
+    setvbuf(stdout,0,2,0);
+    setvbuf(stderr,0,2,0);
+    fd = open("/dev/urandom",0);
+    close(fd);
+    fd = open("./flag",O_RDONLY);
+    if(fd==-1){
+        puts("where is the flag?\n");
+        exit(0);
+    }
+    close(fd);
+    puts("欢迎各位2019级新生来到山东科技大学(泰安)·信息安全协会！");
+    puts("非常期待能遇见喜欢pwn的新生23333");
+    puts("");
+    puts("简单的栈溢出~");
 }
 
 void youwin(){//0x080486b6
-	puts("Wooo~TQL.Take my flag.");
+    puts("Wooo~TQL.Take my flag.");
     system("cat flag");
-	getchar();
-	getchar();
+    getchar();
+    getchar();
 }
 
 void vuln(){
     char buf[40];
-	puts("give me you message :");
+    puts("give me you message :");
     read(0,buf,56);
 }
 
 int main(){
     init();
     vuln();
-	return 0;
+    return 0;
 }
